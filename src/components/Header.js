45cdb45e95e7d799f5d343553566954fd39a2d69
JY Sung <0125sjy@gmail.com>
2024-10-10 12:01:43 +0900

    header

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,18 +5,26 @@ import { useRouter } from "next/router";
 import useSearchStore from "@/store/search_store";
 import Search from "@/pages/search";
 
+
 function Header() {
   const router = useRouter();
+
+  const movePage = (page) => {
+    router.push(page)
+  }
+  
+
   let header;
+
   switch (router.pathname) {
     case "/":
-      header = <HeaderMain />;
+      header = <HeaderMain movePage={movePage} />;
       break;
     case "/category":
-      header = <HeaderMain />;
+      header = <HeaderSub name={'뮤지컬'} />;
       break;
     case "/detail":
-      header = <HeaderMain />;
+      header = <HeaderMain movePage={movePage} />;
       break;
     case "/search":
       header = <HeaderSearch />;
@@ -24,57 +32,69 @@ function Header() {
     case "/search2":
       header = <HeaderSearch />;
       break;
+    case "/login":
+      header = <HeaderLogin />;
+      break;
     default:
-      header = <HeaderMain />;
+      header = <HeaderMain movePage={movePage} />;
       break;
   }
-  return <header>{header}</header>;
+
+  return (
+    <header>
+      {header}
+    </header>
+  );
 }
 
-const HeaderMain = () => {
-  return (
-    <div className={headerStyle.mainHeaderWrap}>
-      <Link href="/">
-        <h1 className="logo"></h1>
-      </Link>
-      <div className={headerStyle.btnWrap}>
-        <Link href="/search">
-          <button type="button" className={headerStyle.search}></button>
-        </Link>
-        <Link href="mypage">
-          <button type="button" className={headerStyle.myPage}></button>
-        </Link>
-      </div>
-    </div>
-  );
-};
-
-const HeaderSub = () => {
-  function goBack() {
+const GoBackBtn = () => {
+  const router = useRouter();
+  const goBack = () => {
     if (window.history.length > 2) {
       router.back(); // 히스토리가 있을 때만 뒤로가기
     } else {
       router.push("/"); // 히스토리가 없으면 홈으로 이동
     }
   }
-  return (
-    <div className={headerStyle.subHeaderWrap}>
-      <h2>
-        <button
+
+  return(
+    <div className={headerStyle.goBackBtnWrap}>
+      <button
           type="button"
           className={headerStyle.goBackBtn}
           onClick={goBack}
-        ></button>
-        마이페이지
-      </h2>
+      ></button>
+    </div>
+  )
+}
+
+//메인 헤더
+const HeaderMain = ({movePage}) => {
+  
+  return (
+    <div className={headerStyle.mainHeaderWrap}>
+      <h1 onClick={() => movePage('/')} className="logo"></h1>
+      <div className={headerStyle.btnWrap}>
+        <button onClick={() => movePage('/search')} type="button" className={headerStyle.search}></button>
+        <button onClick={() => movePage('/mypage')} type="button" className={headerStyle.myPage}></button>
+      </div>
     </div>
   );
 };
 
-const HeaderDetail = () => {
+const HeaderSub = ({name}) => {
   return (
-    <div>
-      <button></button>
+    <div className={headerStyle.subHeaderWrap}>
+      <GoBackBtn />
+      <h2>{name}</h2>
+    </div>
+  );
+};
+
+const HeaderDetail = ({movePage}) => {
+  return (
+    <div className={headerStyle.detailHeaderWrap}>
+      <GoBackBtn />
       <h2></h2>
     </div>
   );
@@ -163,10 +183,11 @@ const HeaderSearch2 = () => {
     </div>
   );
 };
+
 const HeaderMypage = () => {
   return (
     <div className={headerStyle.mypageHeaderWrap}>
-      <button>
+      <button type="button">
         <img src="../../assets/icons/arrow_left.svg" />
       </button>
       <p>마이페이지</p>
@@ -177,4 +198,5 @@ const HeaderMypage = () => {
 const HeaderLogin = () => {
   return <div></div>;
 };
+
 export default Header;
