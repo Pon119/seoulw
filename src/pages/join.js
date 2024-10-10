efb7076df71471278279e67fd81b55587a42d32a
yeon <parkwlfod@naver.com>
2024-10-10 18:25:38 +0900

    login

--- src/pages/join.js
+++ src/pages/join.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Swal from 'sweetalert2'
 import joinStyle from '@/styles/join.module.scss'
+import { collection, addDoc } from "firebase/firestore"; 
 
 
 function Join() {
@@ -23,14 +24,45 @@ function Join() {
     <div className={joinStyle.loginwrap}>
       <h2>회원가입</h2>
       <form>
+        <div className={joinStyle.logininput}>
         <input type='text'  className={joinStyle.inputtext} placeholder='이메일 주소'/>
+        <button type="button" class={`${joinStyle.input_reset_btn} ${joinStyle.inp_button}`}></button>
+        <p class="error_msg"></p>
+        </div>
+        <div className={joinStyle.logininput}>
         <input type='password'  className={joinStyle.inputpass} placeholder='비밀번호 (영문/숫자/특수문자 조합 8~15자) '/>
+        <button type="button" class={`${joinStyle.input_eye_btn} ${joinStyle.inp_button}`}></button>
+        <button type="button" class={`${joinStyle.input_reset_btn} ${joinStyle.inp_button}`}></button>
+        <p class="error_msg"></p>
+        </div>
+        <div className={joinStyle.logininput}>
         <input type='password'  className={joinStyle.inputpass} placeholder='비밀번호 확인'/>
-        <input type='text'  className={joinStyle.inputname} placeholder='성함 '/>
+        <button type="button" class={`${joinStyle.input_eye_btn} ${joinStyle.inp_button}`}></button>
+        <button type="button" class={`${joinStyle.input_reset_btn} ${joinStyle.inp_button}`}></button>
+        <p class="error_msg"></p> 
+        </div>
+       <div className={joinStyle.logininput}>
+       <input type='text'  className={joinStyle.inputname} placeholder='성함 '/>
+       <button type="button" class={`${joinStyle.input_reset_btn} ${joinStyle.inp_button}`}></button>
+       <p class="error_msg"></p> 
+       </div>
+        <div className={joinStyle.logininput}>
         <input type='phone'  className={joinStyle.inputphone} placeholder='휴대폰 번호 ( - 없이 )'/>
-        
+        <button type="button" class={`${joinStyle.input_reset_btn} ${joinStyle.inp_button}`}></button>
+        <p class="error_msg"></p> 
+        </div>
+        <div className={joinStyle.inputcheck}>
         <input type='checkbox' className={joinStyle.checkbox} id="info1" name="info" />
-        <label for="info1"><i></i>개인 정보 수집 및 이용에 동의합니다.</label>
+        <label for="info1"><i></i>이용 약관 동의<span>(필수)</span></label>
+        </div>
+        <div className={joinStyle.inputcheck}>
+        <input type='checkbox' className={joinStyle.checkbox} id="info2" name="info" /> 
+        <label for="info2"><i></i>개인 정보 수집 및 이용 동의<span>(필수)</span></label>
+        </div>
+        <div className={joinStyle.inputcheck}>
+        <input type='checkbox' className={joinStyle.checkbox} id="info3" name="info" />
+        <label for="info3"><i></i>E-mail 및 SMS 광고성 정보 수신 동의<span>(선택)</span></label>
+        </div>
 
         <input type="submit" className={joinStyle.inputbtn} value="가입 완료" onClick={joinPop}/>
       </form>
--- src/pages/login.js
+++ src/pages/login.js
@@ -1,17 +1,56 @@
 import React from 'react'
 import Link from 'next/link'
 import loginStyle from '@/styles/login.module.scss'
+import { useSession, signIn, signOut } from "next-auth/react"
+import { useRouter } from 'next/router';
 
 
 function Login() {
+  const router = useRouter();
+  async function handleSubmit(e){
+    
+    e.preventDefault(); 
+
+    let id=e.target.id.value,
+        password =e.target.pw.value;
+        console.log(id, password);
+
+    //signIn이 계속 새로고침을 함 -> redirect: false로 해야 새로고침 안됨
+    let loginResult = await signIn('credentials',{
+                  redirect:false, id, password
+                 }); 
+      console.log(loginResult);
+      if(loginResult.ok){
+       
+        //홈 화면으로 페이지 이동시
+        const router = useRouter()
+        router.push('/')
+
+
+      }else{
+        console.log("Error", loginResult.error);
+      }
+   }
+   const { data: session } = useSession()
+  //  console.log(session);
+ 
+   if (session) {
+    
+    router.push("mypage");
+   }
+ 
   return (
 
     <div className={loginStyle.loginwrap}>
       <h2>로그인</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
+        <div>
         <input type='Email' label="Email" isRequired className={loginStyle.inputtext} placeholder='이메일 주소'/>
+        <button type="button" class="input_reset_btn input_btn"></button>
+        <p class="error_msg"></p>
+        </div>
         <input type='password'  className={loginStyle.inputpass} placeholder='비밀번호 (영문/숫자/특수문자 조합 8~15자) '/>
-        <input type="submit" value="로그인" />
+        <input type="submit" value="로그인" onClick={() => signIn()} />
         
         <input type='checkbox' className={loginStyle.checkbox} id="chk1" name="chk" defaultChecked/>
         <label htmlFor="chk1"><i></i>아이디 저장</label>
@@ -24,11 +63,12 @@ function Login() {
       <Link href="/join">회원가입</Link>
       </div>
       <div className={loginStyle.loginicon}>
-        <button><img src="../../assets/icons/kakao_icon.svg"/></button>
-        <button><img src="../../assets/icons/naver_icon.svg"/></button>
-        <button><img src="../../assets/icons/google_icon.svg"/></button>
-      </div>
-      
+        <button onClick={()=>signIn('github',{callbackUrl:'/'})}><img src="../../assets/icons/github_icon1.svg"/></button>
+        <button onClick={()=>signIn('naver',{callbackUrl:'/'})}><img src="../../assets/icons/naver_icon.svg"/></button>
+        <button onClick={()=>signIn('google',{callbackUrl:'/'})}><img src="../../assets/icons/google_icon.svg"/></button>
+        {/* 콜백,{callbackUrl:'/'}안넣으면 홈으로 안가고 마이페이지에 남음 */}
+        {/* <button onClick={()=>signIn('google')}><img src="../../assets/icons/google_icon.svg"/></button> */}
+      </div>  
       </div>
   )
 }
