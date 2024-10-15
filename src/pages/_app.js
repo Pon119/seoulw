b97bfb34aa31f10a21e41fd74e32ba48fae9feb4
unknown <0125sjy@gmail.com>
2024-10-15 15:58:42 +0900

    index, app, main_store

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -8,48 +8,50 @@ import Head from 'next/head'
 import { SessionProvider } from "next-auth/react"
 import useMainStore from '@/store/main_store';
 import { fn } from "@/utils/apiFunc";
+import Loading from "@/components/Loading";
+import { fetchData } from "next-auth/client/_utils";
 
 export default function App({
   Component,
   pageProps: { session, ...pageProps },
   }) {
 
-  const {setMainData, mainData} = useMainStore();
+  const {mainData, setMainData} = useMainStore();
   
   useEffect(()=>{
     // fn.search('예술',1)
     // (res=>{
     //   console.log(res)
-
     // });
-
-    setMainData();
+    const fetchMainData = async () => {
+      await setMainData();
+    }
+    fetchMainData();
   },[])
-
-
-
+  
   return (
-    <>
-    <SessionProvider session={session}>
-    <Head>
-      <title>Seoul W</title>
-      <link rel="icon" href="/favicon.ico" />
-      <meta charSet="UTF-8" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <meta name="description" content="Seoul Culture Website" />
-      <meta name="copyright" content="© 2024 Seoul W Website" />
-    </Head>
-    <div className="app">
-      <Header />
-      <Suspense fallback={<main><div>로딩중</div></main>}>
-        <main>
-          <Component {...pageProps} />
-        </main>
-      </Suspense>
-      <Footer />
-      <MenuTapBar />
-    </div>
-    </SessionProvider>
-    </>
+     mainData.length === 0 ? <Loading /> : 
+      <>
+      <SessionProvider session={session}>
+      <Head>
+        <title>Seoul W</title>
+        <link rel="icon" href="/favicon.ico" />
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="description" content="Seoul Culture Website" />
+        <meta name="copyright" content="© 2024 Seoul W Website" />
+      </Head>
+      <div className="app">
+        <Header />
+        <Suspense fallback={<main><div><Loading /></div></main>}>
+          <main>
+            <Component {...pageProps} />
+          </main>
+        </Suspense>
+        <Footer />
+        <MenuTapBar />
+      </div>
+      </SessionProvider>
+      </>
   )
 }
